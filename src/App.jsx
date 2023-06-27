8cdf1f639483b0cd8a33cd6eab79318362467c40
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-06-27 10:35:06 -0500

    Exercise completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { OnionHater } from './components/OnionHater'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Onion Hater</h1>
+        <OnionHater />
+      </div>
+    </>
+  )
+}
+
+export default App
